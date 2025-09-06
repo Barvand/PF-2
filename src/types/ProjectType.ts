f8ed0c4bd4340570e693024e5cc30f600d816e14
B <128028346+Barvand@users.noreply.github.com>
2025-09-06 15:38:34 +0200

    updated TS error

--- src/types/ProjectType.ts
+++ src/types/ProjectType.ts
@@ -2,8 +2,6 @@ export interface Project {
   name: string;
   logo: string;
   description: string;
-  audience: string;
-  design: string;
   techStack: string[];
   mockups: {
     desktop: string;
